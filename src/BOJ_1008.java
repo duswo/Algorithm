9e9140a192645c08587200b2382ebfec2c25de42
YeonJae <lyj00x@naver.com>
2021-07-21 22:58:48 +0900

    BOJ_Algorithm

---
+++ src/BOJ_1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A, B;
+        A = sc.nextDouble();
+        B = sc.nextDouble();
+        System.out.println(A/B);
+    }
+}
